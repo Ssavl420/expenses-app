6bea70c3f79fd1e1e243e537f024214459dc6b99
Sergey Savlevich <sa.savlevich@gmail.com>
2023-05-12 02:24:45 +0700

    add value change limit

--- Js/script.js
+++ Js/script.js
@@ -1,36 +1,72 @@
-const inputFormNode = document.querySelector('[data-find="expensesInput"]');
-const inputBtn = document.querySelector('[data-find="expensesBtn"]');
+const inputFormLimitNode = document.querySelector('[data-find="limitInput"]');
+const inputFormExpensesNode = document.querySelector('[data-find="expensesInput"]');
+// const inputBtn = document.querySelector('[data-find="expensesBtn"]');
+const limitBtn = document.querySelector('[data-find="LimitBtn"]');
 const historyNode = document.querySelector('[data-find="history"]')
-const form = document.querySelector('.form');
+const formExpenses = document.querySelector('[data-find="form-expenses"]');
+const formLimit = document.querySelector('[data-find="form-limit"]');
 const sumNode = document.querySelector('[data-find="sum"]');
+const limitNode = document.querySelector('[data-find="limit"]');
+// let limit = +inputFormLimitNode.value.replace("," , ".");
 
 let expenses = [];
 
-form.addEventListener('submit', function (e) {
+formLimit.addEventListener('submit', function (e) {
   e.preventDefault();
-  if (!inputFormNode.value) {
+  if (!inputFormLimitNode.value) {
     return;
-  } 
+  }
+  const limit = +inputFormLimitNode.value.replace("," , ".");
+  formLimit.classList.add("js-closed");
+  formExpenses.classList.add("js-open");
 
-  const history = document.querySelector('.__history');
-  history.classList.add('js-active');
+  const limInfo = document.querySelector('.info__limit');
+  limInfo.classList.add('js-active');
+
+  console.log(limit)
+
+  limitNode.innerText = (limit + ' \u20bd');
+
+  inputFormExpensesNode.value = "";
+
+  // ----------------------------------
+  formExpenses.addEventListener('submit', function (e) {
+    e.preventDefault();
+    if (!inputFormExpensesNode.value) {
+      return;
+    } 
   
-  const info = document.querySelector('.__info');
-  info.classList.add('js-active');
-
-  const expense = +inputFormNode.value.replace("," , ".");
-  expenses.push(expense);
+    const history = document.querySelector('.__history');
+    history.classList.add('js-active');
+    
+    const sumInfo = document.querySelector('.info__sum');
+    sumInfo.classList.add('js-active');
   
-  inputFormNode.value = '';
+    const expense = +inputFormExpensesNode.value.replace("," , ".");
+    expenses.push(expense);
+    
+    inputFormExpensesNode.value = '';
+  
+    let expensesListHTML = '';
+    let sum = 0;
+    expenses.forEach(element => {
+      expensesListHTML += `<li>${element} &#8381</li>`;
+      sum += element;
+    });
+    
+    historyNode.innerHTML = `<ol>${expensesListHTML}</ol>`;
+  
+    sumNode.innerText = (sum + ' \u20bd');
 
-  let expensesListHTML = '';
-  let sum = 0;
-  expenses.forEach(element => {
-    expensesListHTML += `<li>${element} &#8381</li>`;
-    sum += element;
+    console.log(expense)
+
   });
-  
-  historyNode.innerHTML = `<ol>${expensesListHTML}</ol>`;
 
-  sumNode.innerText = (sum + ' \u20bd');
+  const limitChange = document.querySelector(".js-info__limit-change");
+  limitChange.addEventListener('click', function () {
+    formLimit.classList.remove("js-closed");
+    formExpenses.classList.remove("js-open");
+    inputFormLimitNode.value = "";
+  })
+  // -----------------------------------
 });
